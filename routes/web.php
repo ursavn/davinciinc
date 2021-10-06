15e2c2354c409bcf3bb5339fe28898a8005067a0
Nam Nguyen <nam.nguyen@ursa.vn>
2021-10-06 18:27:42 +0700

    select template page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/select-template', [TemplateController::class, 'getAllTemplates'])->name('select-template');
