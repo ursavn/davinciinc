9808230abaab7b997e31cd559a47782dbe5febde
Van Pham <van.pham@ursa.vn>
2021-10-06 17:07:52 +0700

    update model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
 
     protected $table = 'user';
 
-    protected $primaryKey ='id';
+    protected $primaryKey = 'id';
 
     protected $fillable = [
         'username',
