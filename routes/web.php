70acfeebeba90559ab6b36dff1ea6fbc508b5f95
Nam Nguyen <nam.nguyen@ursa.vn>
2021-10-06 18:32:06 +0700

    remove duplicated import

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TemplateController;
 
 /*
 |--------------------------------------------------------------------------
